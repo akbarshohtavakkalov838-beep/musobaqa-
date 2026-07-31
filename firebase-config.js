4461364049e839af61ddbb02344eeab1d3aa3e03
akbarshohtavakkalov838-beep <akbarshohtavakkalov838@gmail.com>
2026-07-31 21:47:38 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -13,13 +13,13 @@
    =========================================================== */
 
 const firebaseConfig = {
-  apiKey: "SIZNING_API_KEYINGIZ",
-  authDomain: "loyiha-nomi.firebaseapp.com",
-  databaseURL: "https://mockbaza-reyting-default-rtdb.firebaseio.com",
-  projectId: "loyiha-nomi",
-  storageBucket: "loyiha-nomi.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:xxxxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyDvSeSdQ-_vR69kjb2mgjifLWqs6IerV9w",
+  authDomain: "mockbaza-reyting.firebaseapp.com",
+  projectId: "mockbaza-reyting",
+  storageBucket: "mockbaza-reyting.firebasestorage.app",
+  messagingSenderId: "1094359427655",
+  appId: "1:1094359427655:web:0008d65e28cd01d02a9f62",
+  measurementId: "G-1P2VSVBEBR"
 };
 
 firebase.initializeApp(firebaseConfig);
