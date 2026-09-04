66a2edb3beedaa5d14c8639bc3723d18afcca450
akbarshohtavakkalov838-beep <akbarshohtavakkalov838@gmail.com>
2026-09-05 01:21:46 +0500

    Create firebase-config.js

---
+++ portal/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// Loyiha: mockbaza-reyting (eski musobaqa- loyihangiz bilan bir xil Firebase project)
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDvSeSdQ-_vR69kjb2mgjifLWqs6IerV9w",
+  authDomain: "mockbaza-reyting.firebaseapp.com",
+  projectId: "mockbaza-reyting",
+  storageBucket: "mockbaza-reyting.firebasestorage.app",
+  messagingSenderId: "1094359427655",
+  appId: "1:1094359427655:web:0008d65e28cd01d02a9f62"
+};
